0b3593a40ae7fcdb0ae64ed2614c9cf03101c2db
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2024-11-04 22:29:40 +0100

    user-details-service microservice implementation

--- user-details-service-tma/src/controllers/Controller.ts
+++ user-details-service-tma/src/controllers/Controller.ts
@@ -1,36 +1,27 @@
 import { Request, Response } from 'express';
 import { Collection, ObjectId } from 'mongodb';
-import { logger } from '../utils/logs';
 import { UserType } from '../models/User';
-import { IController } from '../interfaces/IControllerInterface';
 
-export class UserController implements IController {
-  endpoint = process.env.USERS_SERVICE!;
+export class UserController {
   constructor(private collection: Collection<UserType>) {}
 
-  public async getUserId(req: Request, res: Response): Promise<void> {
-    console.log(req.params.id);
+  public async getUserById(req: Request, res: Response): Promise<void> {
+    const userId = req.params.id;
+    console.log(userId);
+
     try {
-      const result = await this.collection.findOne(
-        { _id: req.params.id as unknown as ObjectId },
-        { projection: { profile: 1 } }
+      const user = await this.collection.findOne(
+        { _id: new ObjectId(userId) },
+        { projection: { _id: 1, name: 1, email: 1, createdDate: 1, address: 1, phone: 1 } }
       );
-      res.status(200).json(result);
+
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
     } catch (error) {
-      res.json({ status: '400', message: error });
+      res.status(400).json({ status: '400', message: error });
     }
   }
-
-  //   public async getUserData(req: Request, res: Response): Promise<void> {
-  //     try {
-  //       const userId = new ObjectId(req.params.id);
-  //       // const userId = req.params.id as unknown as ObjectId
-  //       console.log('userId ', userId);
-  //       const result = await this.accountsCollection.findOne({ userId: userId });
-  //       logger('info', JSON.stringify(result));
-  //       res.status(200).json(result);
-  //     } catch (error) {
-  //       res.json({ status: '400', message: error });
-  //     }
-  //   }
 }
